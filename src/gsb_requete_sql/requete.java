f9af060c23a9853026d28c08ddcc395a8a0afc34
Yohan <yohan.albuquerque7@gmail.com>
2020-02-18 12:24:35 +0100

    Modif requete SQL

--- src/gsb_requete_sql/requete.java
+++ src/gsb_requete_sql/requete.java
@@ -1,6 +1,7 @@
 package gsb_requete_sql;
 import java.sql.*;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.List;
 
 import gsb_hibernate.*;
@@ -15,7 +16,7 @@ public class requete {
 					System.out.println("Java JDBC");
     	Connecter();
 		InsererDonnees(null, null, null);
-		SuppDonnees();
+		SuppDonnees(null);
 		// ModifierDonnees();
 		LireDonnees();
 		LireDonneesOrdreAlpha();
@@ -40,21 +41,23 @@ public class requete {
 		}
 	}
 	
-	public static void LireDonnees(){
+	public static List<String> LireDonnees(){
 		Statement stmt;
 		ResultSet rs;
+		List<String> resultat = new ArrayList<String>();
 		try {
 		stmt=connection.createStatement();
 		String sql=("select * from utilisateur");
 		rs=stmt.executeQuery(sql);
 		System.out.println("Données présente en base :");
 		while(rs.next()) {
-			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+			resultat.add(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3)+" "+rs.getString(4));
+			
 		}
 	}catch (SQLException e) {
 		e.printStackTrace();
 	}
-	
+		return resultat;
 	}
 	
 	public static void LireDonneesOrdreAlpha(){
@@ -93,17 +96,17 @@ public class requete {
 		}
 	}
 	
-	public static void SuppDonnees() {
+	public static void SuppDonnees(Integer id) {
 		Statement stmt;
-		System.out.println("Saisir l'id du compte a supprimer");
+		/*System.out.println("Saisir l'id du compte a supprimer");
 		Scanner sc = new Scanner(System.in);
 		System.out.print( "Id : " );
-        Integer id = sc.nextInt();
+        Integer id = sc.nextInt();*/
 		try {
 			stmt=connection.createStatement();
 			String sql="DELETE FROM utilisateur WHERE id='"+id+"'";
 			stmt.executeUpdate(sql);
-			System.out.println("Suppression du compte possedant l'id '"+id+"' réussi");
+			// System.out.println("Suppression du compte possedant l'id '"+id+"' réussi");
 		}catch (SQLException e){
 			e.printStackTrace();
 		}
