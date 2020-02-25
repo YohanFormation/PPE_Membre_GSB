f74c0c0a1c23e03baf01236a36c4e0aafaf6fb2d
Yohan <yohan.albuquerque7@gmail.com>
2020-02-25 11:08:52 +0100

    + Rafraichissement des données

--- src/gsb_interface/LireUtilisateur.java
+++ src/gsb_interface/LireUtilisateur.java
@@ -8,17 +8,28 @@ import javax.swing.JTextPane;
 import javax.swing.JTable;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
 
 public class LireUtilisateur extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
+	JTextArea text_fonction = new JTextArea();
 	public LireUtilisateur() {
 		requete co = new requete();
 		setLayout(null);
-		
-		JTextArea text_fonction = new JTextArea();
+		text_fonction.setEditable(false);
+		text_fonction.setBounds(12, 13, 570, 514);
+		add(text_fonction);
+		String result = ("Id" + "  " + "Nom" + "  " + "Prenom" + "  " + "Fonction");
+		String newLine = System.getProperty("line.separator");
+		for (String iterable_element : co.LireDonnees()) {
+			result = result + "\n" + iterable_element;
+		}
+		text_fonction.setText(result);
+	}
+
+	public void AcutaliserDonnees() {
+		requete co = new requete();
+		setLayout(null);
 		text_fonction.setEditable(false);
 		text_fonction.setBounds(12, 13, 570, 514);
 		add(text_fonction);
